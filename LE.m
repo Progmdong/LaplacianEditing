cb2911c93fa88b620e5e1b7981103c1eef705570
Progmdong <progmdong@163.cm>
2013-11-13 20:09:46 +0800

    Edited in 11.13

--- LE.m
+++ LE.m
@@ -1,23 +1,27 @@
-% L_prime is the Matrix returned by Preprocessor
-function V_final_xyz = LE(L_prime,V,static_anchors,handle_anchors,handle_anchors_pos)
+%L_prime (3n,3n)     the matrix returned by preprocessor
+%V (n,3)                    original vertex coord
+%static_anchors       index of static points
+%handle_anchors     index of handle points
+%handle_new_pos    new position of handle points
+%w                             weight of position coord influence against Laplacian coord
+
+function V_prime= LE(L_prime,V,static_anchors,handle_anchors,handle_new_pos,w)
+
 anchors = [static_anchors handle_anchors];
-V_pi = V;
 n=length(V);
-V_pi(handle_anchors,:) = handle_anchors_pos;
-A_prime=L_prime;
-rhs = zeros(3*n,1);
-for j=1:length(anchors)
-    A_prime=[A_prime
-        (1:(3*n))==anchors(j)
-        (1:(3*n))==(anchors(j)+n)
-        (1:(3*n))==(anchors(j)+2*n) ];
-    rhs=[rhs
-        V_pi(anchors(j),1)
-        V_pi(anchors(j),2)
-        V_pi(anchors(j),3)];
-end
+V(handle_anchors,:)=handle_new_pos;
+U= reshape(V,3*n,1);
+
+anchor_vec = sparse(1,3*n);
+anchor_vec([anchors,anchors+n,anchors+2*n]) = 1;
+anchor_mat = diag(anchor_vec);
+
+B_prime = anchor_mat'*anchor_mat;
+rhs = B_prime*U;
+
+
+A_prime=L_prime+B_prime;
 
 V_final = A_prime\rhs;
-rhs_re = A_prime*V_final;
-V_final_xyz = [V_final(1:n) V_final((n+1):(2*n)) V_final((2*n+1):(3*n)) ];
+V_prime = [V_final(1:n) V_final((n+1):(2*n)) V_final((2*n+1):(3*n)) ];
 end
